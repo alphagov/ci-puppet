f0808a2b54a1024a98c51cebb32e997423e6956c
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-07-02 11:27:37 +0100

    Remove shared jenkins keys

--- modules/ci_environment/manifests/jenkins_master.pp
+++ modules/ci_environment/manifests/jenkins_master.pp
@@ -1,30 +1,3 @@
 #Class to install things only on the Jenkins Master
-class ci_environment::jenkins_master (
-    $ssh_private_key = undef,
-) {
-    if ($ssh_private_key) {
-        # Install the SSH private key for Jenkins
-        file { '/home/jenkins':
-            ensure  => directory,
-            mode    => '0700',
-            owner   => 'jenkins',
-            group   => 'jenkins',
-            require => Class['jenkins'],
-        }
-        file { '/home/jenkins/.ssh':
-            ensure  => directory,
-            mode    => '0700',
-            owner   => 'jenkins',
-            group   => 'jenkins',
-            require => File['/home/jenkins'],
-        }
-        file { '/home/jenkins/.ssh/id_rsa':
-            ensure  => present,
-            content => $ssh_private_key,
-            mode    => '0600',
-            owner   => 'jenkins',
-            group   => 'jenkins',
-            require => File['/home/jenkins/.ssh'],
-        }
-    }
+class ci_environment::jenkins_master {
 }
