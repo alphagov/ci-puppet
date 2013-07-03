37148abd33827abc940ad2d365953dca0b39884d
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-07-03 11:26:19 +0100

    [#48737855] Secure Jenkins with SSL through NginX

--- Puppetfile
+++ Puppetfile
@@ -6,3 +6,7 @@ mod 'torrancew/account',    '0.0.3'
 
 mod 'jenkins',  :git => 'git://github.com/alphagov/puppet-jenkins.git',
                 :ref => '149f22d119ec8851868c4fa6ee0c2c0db01ba398'
+mod 'nginx',    :git => 'git://github.com/alphagov/puppet-nginx.git',
+                :ref => 'c5737d68d24c48a644917544f1958279a58b9a19'
+mod 'ssl',      :git => 'git://github.com/alphagov/puppet-ssl.git',
+                :ref => '23bbb5ab57f26269acce3d4b43e643781747a551'
--- modules/ci_environment/manifests/jenkins_master.pp
+++ modules/ci_environment/manifests/jenkins_master.pp
@@ -1,4 +1,20 @@
 #Class to install things only on the Jenkins Master
 class ci_environment::jenkins_master {
+
     Package <| title == 'jenkins' |> -> Jenkins::Plugin <| |>
+
+    package {'ssl-cert':
+      ensure  => latest,
+      require => Exec['apt-get-update'],
+    }
+
+    class {'nginx::server':
+      require => Package['ssl-cert'],
+    }
+
+    nginx::vhost::proxy  { 'jenkins-nginx':
+        ssl            => true,
+        ssl_redirect   => true,
+        isdefaultvhost => true,
+    }
 }
