1a5a649de6c75ff303a7fef7ebc5673833899fe7
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-07-15 16:07:38 +0100

    [#52299619] collectd config to send to carbon & basic plugins

--- modules/gds_collectd/manifests/init.pp
+++ modules/gds_collectd/manifests/init.pp
@@ -1,6 +1,20 @@
-# including collectd
+# setting up collectd to write data to the management box
+#   and the plugs in we want.
+#
+# The default syslog level in info, incase that gets annoying
+#
 class gds_collectd {
-  include collectd
-  include collectd::plugin::write_graphite
+  class { '::collectd':
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
+
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => 'ci-management-1',
+  }
+
+  collectd::plugin {'syslog':}
+  collectd::plugin {'memory':}
   collectd::plugin {'cpu':}
 }
