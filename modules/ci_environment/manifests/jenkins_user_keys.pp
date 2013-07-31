2da05128350deb67234c2f80d6ca42c1440d69aa
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-07-31 14:42:05 +0100

    [#52858717]removing unnecessary puppet code

--- modules/ci_environment/manifests/jenkins_user_keys.pp
+++ modules/ci_environment/manifests/jenkins_user_keys.pp
@@ -7,11 +7,6 @@ class ci_environment::jenkins_user_keys {
   $private_key = "${jenkins_home}/.ssh/id_rsa"
   $public_key = "${jenkins_home}/.ssh/id_rsa.pub"
 
-  file { $public_key:
-    checksum => md5,
-    require  => [ User['jenkins'], File["${jenkins_home}/.ssh"] ],
-  }
-
   file { "${jenkins_home}/.ssh":
     ensure => directory,
     mode   => '0600',
