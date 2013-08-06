1f204413d817e64b7f333fcb2bbc04fe33a8b1d7
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-08-06 12:04:32 +0100

    [#53062579] changing the whitespace to be consistant accross the puppet files

--- modules/ci_environment/manifests/jenkins_master.pp
+++ modules/ci_environment/manifests/jenkins_master.pp
@@ -13,64 +13,64 @@ class ci_environment::jenkins_master (
   $jenkins_serveraliases = [],
   $slave_user = 'slave'
 ) {
-    validate_string($github_enterprise_cert, $jenkins_servername)
-    validate_array($jenkins_serveraliases)
+  validate_string($github_enterprise_cert, $jenkins_servername)
+  validate_array($jenkins_serveraliases)
 
-    include jenkins
-    include jenkins_user
+  include jenkins
+  include jenkins_user
 
-    Class['jenkins'] -> Class['jenkins_user']
-    Package <| title == 'jenkins' |> -> Jenkins::Plugin <| |>
+  Class['jenkins'] -> Class['jenkins_user']
+  Package <| title == 'jenkins' |> -> Jenkins::Plugin <| |>
 
-    package {'ssl-cert':
-        ensure  => '1.0.28ubuntu0.1',
-        require => Exec['apt-get-update'],
-    }
+  package {'ssl-cert':
+    ensure  => '1.0.28ubuntu0.1',
+    require => Exec['apt-get-update'],
+  }
 
-    class {'nginx::server':
-        require => Package['ssl-cert'],
-    }
+  class {'nginx::server':
+    require => Package['ssl-cert'],
+  }
 
-    nginx::vhost::proxy  { 'jenkins-nginx':
-        ssl            => true,
-        ssl_redirect   => true,
-        isdefaultvhost => true,
-        servername     => $jenkins_servername,
-        serveraliases  => $jenkins_serveraliases,
-    }
+  nginx::vhost::proxy  { 'jenkins-nginx':
+    ssl            => true,
+    ssl_redirect   => true,
+    isdefaultvhost => true,
+    servername     => $jenkins_servername,
+    serveraliases  => $jenkins_serveraliases,
+  }
 
-    # This file resource installs a Jenkins plugin manually. The build we are
-    # using is from a Pull Request and has not been merged into mainline. With
-    # any luck, when version 0.14 is release, we can just use that.
-    # Download URL:
-    #   https://buildhive.cloudbees.com/job/jenkinsci/job/github-oauth-plugin
-    #          /34/org.jenkins-ci.plugins$github-oauth/
-    #
-    file {'/var/lib/jenkins/plugins/github-oauth.hpi':
-        ensure => 'present',
-        owner  => 'jenkins',
-        group  => 'nogroup',
-        mode   => '0644',
-        source => 'puppet:///modules/ci_environment/jenkins-plugin-github-oauth-0.14-b34.hpi',
-        notify => Class['jenkins::service'],
-    }
+  # This file resource installs a Jenkins plugin manually. The build we are
+  # using is from a Pull Request and has not been merged into mainline. With
+  # any luck, when version 0.14 is release, we can just use that.
+  # Download URL:
+  #   https://buildhive.cloudbees.com/job/jenkinsci/job/github-oauth-plugin
+  #          /34/org.jenkins-ci.plugins$github-oauth/
+  #
+  file {'/var/lib/jenkins/plugins/github-oauth.hpi':
+    ensure => 'present',
+    owner  => 'jenkins',
+    group  => 'nogroup',
+    mode   => '0644',
+    source => 'puppet:///modules/ci_environment/jenkins-plugin-github-oauth-0.14-b34.hpi',
+    notify => Class['jenkins::service'],
+  }
 
-    file {'/etc/ssl/certs/github.gds.pem':
-        ensure  => 'present',
-        content => $github_enterprise_cert,
-        notify  => Exec['import-github-cert'],
-    }
+  file {'/etc/ssl/certs/github.gds.pem':
+    ensure  => 'present',
+    content => $github_enterprise_cert,
+    notify  => Exec['import-github-cert'],
+  }
 
-    exec {'import-github-cert':
-        command => '/usr/bin/keytool -import -trustcacerts -alias github.gds \
-                    -file /etc/ssl/certs/github.gds.pem \
-                    -keystore /etc/ssl/certs/java/cacerts \
-                    -storepass changeit -noprompt',
-        unless  => '/usr/bin/keytool -list \
-                    -keystore /etc/ssl/certs/java/cacerts \
-                    -storepass changeit | grep github.gds',
-    }
+  exec {'import-github-cert':
+    command => '/usr/bin/keytool -import -trustcacerts -alias github.gds \
+                -file /etc/ssl/certs/github.gds.pem \
+                -keystore /etc/ssl/certs/java/cacerts \
+                -storepass changeit -noprompt',
+    unless  => '/usr/bin/keytool -list \
+                -keystore /etc/ssl/certs/java/cacerts \
+                -storepass changeit | grep github.gds',
+  }
 
-    jenkins::api_user { $slave_user: }
-    jenkins::api_user { 'pingdom': }
+  jenkins::api_user { $slave_user: }
+  jenkins::api_user { 'pingdom': }
 }
--- modules/ci_environment/manifests/jenkins_slave.pp
+++ modules/ci_environment/manifests/jenkins_slave.pp
@@ -6,9 +6,9 @@
 #
 class ci_environment::jenkins_slave {
 
-    include java
-    include jenkins::slave
-    include jenkins_user
+  include java
+  include jenkins::slave
+  include jenkins_user
 
-    Exec['apt-get-update'] -> Class['java'] -> Class['jenkins::slave'] -> Class['jenkins_user']
+  Exec['apt-get-update'] -> Class['java'] -> Class['jenkins::slave'] -> Class['jenkins_user']
 }
