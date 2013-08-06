86b162c34066a6ecae1183397a13ffc83e133de8
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-08-06 16:27:08 +0100

    [#53062579] added dependancies for opsmanual
    opsmanual needs VirtualEnv

--- modules/ci_environment/manifests/jenkins_slave.pp
+++ modules/ci_environment/manifests/jenkins_slave.pp
@@ -11,4 +11,15 @@ class ci_environment::jenkins_slave {
   include jenkins_user
 
   Exec['apt-get-update'] -> Class['java'] -> Class['jenkins::slave'] -> Class['jenkins_user']
+
+  package { [
+    'python-virtualenv', # needed for infrastructure::opsmanual
+    ]:
+      ensure => installed,
+  }
+
+  package { 'bundler':
+    ensure   => '1.1.4',
+    provider => 'gem',
+  }
 }
