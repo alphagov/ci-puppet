8fa5d21419dc926867bf2e5b915e176df464112b
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-10-01 11:12:05 +0100

    Agree to the Oracle license using debconf
    Fixes #54.

--- modules/ci_environment/manifests/jenkins_job_support.pp
+++ modules/ci_environment/manifests/jenkins_job_support.pp
@@ -72,6 +72,19 @@ class ci_environment::jenkins_job_support {
     require => Class['clamav'],
   }
 
+  # Agree to the Oracle license agreement, so that we can install Java 7, so
+  # that we can install ElasticSearch
+  exec {
+    'set-licence-selected':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+    'set-licence-seen':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+  }
+  package { 'oracle-java7-installer':
+    ensure  => present,
+    require => [Exec['set-licence-selected'], Exec['set-licence-seen']],
+  }
+
   class { 'elasticsearch':
     version            => "0.20.6-ppa1~${::lsbdistcodename}1",
     number_of_replicas => '0'
