2f309da49b7d755bd3f137511f737cfe663c9fdd
Matt Bostock <matt.bostock@digital.cabinet-office.gov.uk>
2013-12-31 13:14:13 +0000

    [#60949022] Move Fail2Ban config to its own class
    - Move Fail2Ban configuration to its own class to avoid polluting the
    ci_environment base.pp manifest.
    - Replace hardcoded IPs to be whitelisted by Fail2Ban with a hiera
    array.
    - Notify Fail2Ban service when jail.local file is updated.

---
+++ modules/ci_environment/manifests/fail2ban.pp
@@ -0,0 +1,22 @@
+# == Class: ci_environment::fail2ban
+#
+# Installs fail2ban for IP blacklisting
+#
+class ci_environment::fail2ban {
+  $whitelist_ips = hiera('ci_environment::fail2ban::whitelist_ips')
+
+  class { '::fail2ban':
+    require => Exec['apt-get-update']
+  }
+
+  file { '/etc/fail2ban/jail.local':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('ci_environment/etc/fail2ban/jail.local.erb'),
+    notify  => Service['fail2ban'],
+    # Require package rather than class to avoid dependency issues
+    # incurred by the Fail2Ban module we are using.
+    require => Package['fail2ban'],
+  }
+}
