5304d4f2af2ed9c687f9806c8c0127dca7079c77
Dan Carley <dan.carley@gmail.com>
2014-01-14 12:10:39 +0000

    [#59323898] cdn_logs module for redirector
    Configures rsyslog with a TCP input which logs to a single file. It presents
    an SSL certificate to allow others to verify who the server is, but not to
    verify who the clients are. Firewall does not restrict specific addresses -
    that's done by the VSE.

---
+++ modules/cdn_logs/manifests/init.pp
@@ -0,0 +1,50 @@
+# == Class: cdn_logs
+#
+# rsyslog receiver for CDN access logs.
+#
+class cdn_logs (
+  $port = 50514,
+  $key,
+  $key_file = '/etc/ssl/rsyslog.key',
+  $cert,
+  $cert_file = '/etc/ssl/rsyslog.crt',
+  $log_dir = '/srv/logs/cdn',
+) {
+  file { $log_dir:
+    ensure => directory,
+    owner  => 'syslog',
+  }
+
+  file { $key_file:
+    ensure  => file,
+    content => $key,
+    owner   => 'syslog',
+    mode    => '0400',
+    notify  => Class['rsyslog::service'],
+  }
+  file { $cert_file:
+    ensure  => file,
+    content => $cert,
+    owner   => 'syslog',
+    mode    => '0444',
+    notify  => Class['rsyslog::service'],
+  }
+
+  rsyslog::snippet { '10-cdn_remote':
+    content   => template('cdn_logs/etc/rsyslog.d/10-cdn_remote.conf.erb'),
+    require   => [
+      # Parent log dir and rsyslog configs.
+      Class['ci_environment::transition_logs'],
+      File[
+        $key_file,
+        $cert_file
+      ]
+    ],
+  }
+
+  ufw::allow { 'rsyslog_cdn_logs':
+    port  => $port,
+    ip    => 'any',
+    proto => 'tcp',
+  }
+}
