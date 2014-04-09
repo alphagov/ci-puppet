31578e50333884ef532386a7d77936041837fc08
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-04-09 10:28:21 +0100

    Remove unnecessarily vendored GitHub OAuth plugin
    I've installed this plugin on CI using Jenkins and have restarted it
    to take effect.
    
    This reverts commit 065b7002e4804.
    
    Fixes #86.

--- modules/ci_environment/manifests/jenkins_master.pp
+++ modules/ci_environment/manifests/jenkins_master.pp
@@ -41,22 +41,6 @@ class ci_environment::jenkins_master (
     serveraliases  => $jenkins_serveraliases,
   }
 
-  # This file resource installs a Jenkins plugin manually. The build we are
-  # using is from a Pull Request and has not been merged into mainline. With
-  # any luck, when version 0.14 is release, we can just use that.
-  # Download URL:
-  #   https://buildhive.cloudbees.com/job/jenkinsci/job/github-oauth-plugin
-  #          /34/org.jenkins-ci.plugins$github-oauth/
-  #
-  file {"${jenkins_home}/plugins/github-oauth.hpi":
-    ensure => 'present',
-    owner  => 'jenkins',
-    group  => 'nogroup',
-    mode   => '0644',
-    source => 'puppet:///modules/ci_environment/jenkins-plugin-github-oauth-0.14-b34.hpi',
-    notify => Class['jenkins::service'],
-  }
-
   file {'/etc/ssl/certs/github.gds.pem':
     ensure  => 'present',
     content => $github_enterprise_cert,
