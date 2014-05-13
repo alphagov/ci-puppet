d0367c6c3c7f616dcdad1ce5b67fd8374ec76f1a
bob walker <bob@randomness.org.uk>
2014-05-13 14:12:17 +0100

    [#69996276] Set up disks using LVM
    Specify pv/vg/lvs without using the shortcut of lvm::volume because we
    have two logical volumes and in the long run there will be another
    physical volume in the volume group.
    
    Using exents for the cdn logical volume because lvcreate makes 128G one
    more extent than is available.

---
+++ modules/ci_environment/manifests/transition_logs/disks.pp
@@ -0,0 +1,51 @@
+#class to set up disks on transition-logs-1
+class ci_environment::transition_logs::disks {
+
+  physical_volume { '/dev/sdc1':
+    ensure => present,
+  }
+
+  volume_group { 'logs':
+    ensure           => present,
+    physical_volumes => '/dev/sdc1',
+  }
+
+  logical_volume { 'remote_users':
+    ensure       => present,
+    volume_group => 'logs',
+    size         => '384G',
+
+  }
+
+  filesystem { '/dev/logs/remote_users':
+    ensure  => present,
+    fs_type => 'ext4',
+    require => Logical_volume['remote_users'],
+  }
+
+  ext4mount { '/srv/logs/log-1':
+    disk         => '/dev/mapper/logs-remote_users',
+    mountoptions => 'defaults',
+    mountpoint   => '/srv/logs/log-1',
+    require      => Filesystem['/dev/logs/remote_users'],
+  }
+
+  logical_volume { 'cdn':
+    ensure       => present,
+    volume_group => 'logs',
+    extents      => '32767',
+  }
+
+  filesystem { '/dev/logs/cdn':
+    ensure  => present,
+    fs_type => 'ext4',
+  }
+  ext4mount { '/srv/logs/log-1/cdn':
+    disk         => '/dev/mapper/logs-cdn',
+    mountoptions => 'defaults',
+    mountpoint   => '/srv/logs/log-1/cdn',
+    require      => [Filesystem['/dev/logs/cdn'],Ext4mount['/srv/logs/log-1']],
+  }
+
+
+}
