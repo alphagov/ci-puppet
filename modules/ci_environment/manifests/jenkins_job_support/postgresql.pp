44d4e15962eaa6e8e5775c251367f9f247d7c383
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-01 16:04:19 +0100

    Postgres support for Transition apps
    Adds the same module we use in GOV.UK puppet and then creates a database
    for the Transition app to use.

---
+++ modules/ci_environment/manifests/jenkins_job_support/postgresql.pp
@@ -0,0 +1,21 @@
+# == Class: ci_environment::jenkins_job_support::postgresql
+# Installs postgresql on the server
+class ci_environment::jenkins_job_support::postgresql {
+  include postgresql::server
+  include postgresql::server::contrib
+  include postgresql::lib::devel
+
+  postgresql::server::db { 'transition_test':
+    encoding => 'UTF8',
+    owner    => 'transition',
+    password => postgresql_password('transition', 'transition'),
+    user     => 'transition',
+  }
+
+  exec { 'Load pgcrypto for postgres db transition_test':
+    user    => 'postgres',
+    command => 'psql -d transition_test -c "CREATE EXTENSION IF NOT EXISTS pgcrypto"',
+    # TODO: Need an unless to test if extension is already present
+    require => Postgresql::Server::Db['transition_test'],
+  }
+}
