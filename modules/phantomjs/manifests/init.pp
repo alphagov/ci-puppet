1bab34c7e62c0fbe42e50056148a8ecc0ddda7a8
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-24 10:36:45 +0100

    Remove FIXME now that PhantomJS is from our PPA

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -5,9 +5,4 @@ class phantomjs {
   package { 'phantomjs':
     ensure => '1.9.7-0~ppa1',
   }
-
-  # FIXME: remove when this has been run everywhere
-  file { '/usr/local/bin/phantomjs':
-    ensure  => absent,
-  }
 }
