0c0a720350a5c42e8145594ecd04cdb21b94d234
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-24 10:36:03 +0100

    Update documentation for PhantomJS
    We stopped installing this from S3 as of 705613c824c150ed363b43556a0b76b66154c850,
    so this comment is incorrect.

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: phantomjs
 #
-# Installs phantomjs, by downloading from our s3 bucket.
+# Installs PhantomJS from our PPA.
 class phantomjs {
   package { 'phantomjs':
     ensure => '1.9.7-0~ppa1',
