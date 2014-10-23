f99c75236a1a87a04d23070df8ae50a047242d10
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-10-23 14:59:10 +0100

    Ensure apt-get update runs before package installs.
    Without this, apt-get update can run after attempting to install a
    package, meaning that the package install can fail.
    
    This is lifted straight from the main puppet repo.

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,16 @@ import 'nodes'
 Exec {
   path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+
+# Ensure update is always run before any package installs.
+# title conditions prevent a dependency loop within apt module.
+Class['apt::update'] -> Package <|
+  provider != pip and
+  provider != gem and
+  provider != system_gem and
+  ensure != absent and
+  ensure != purged and
+  title != 'python-software-properties' and
+  title != 'software-properties-common' and
+  tag != 'no_require_apt_update'
+|>
