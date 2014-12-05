b2de936368a2bc538e93f32d08cbf801f7a60197
Matt Bostock <matt@mattbostock.com>
2014-12-05 10:58:50 +0000

    Merge site.pp and nodes.pp
    To fix the deprecation message warning that `import` is deprecated in
    newer versions of Puppet:
    
        The use of 'import' is deprecated at /Users/matt/govuk/ci-puppet/manifests/site.pp:3. See http://links.puppetlabs.com/puppet-import-deprecation

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
-import 'nodes'
+# Use hiera as a lightweight ENC.
+$machine_role = regsubst($::clientcert, '^(.*)-\d\..*$', '\1')
+$machine_id = regsubst($::clientcert, '^.*-(\d)\..*$', '\1')
+node default {
+  hiera_include('classes')
+}
 
 Exec {
   path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
