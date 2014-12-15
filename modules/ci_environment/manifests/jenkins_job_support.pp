937b7c5bb890103808eebf1c0aacefbaf847f5a3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-12-15 11:47:39 +0000

    Add Go 1.4 to CI servers

--- modules/ci_environment/manifests/jenkins_job_support.pp
+++ modules/ci_environment/manifests/jenkins_job_support.pp
@@ -46,7 +46,7 @@ class ci_environment::jenkins_job_support {
   class { 'goenv':
     global_version => '1.2.2',
   }
-  goenv::version { ['1.2.2', '1.3.1', '1.3.3']: }
+  goenv::version { ['1.2.2', '1.3.1', '1.3.3', '1.4']: }
   package { ['golang-gom', 'godep']:
     ensure => latest,
   }
