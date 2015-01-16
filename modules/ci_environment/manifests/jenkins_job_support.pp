a720f8889d83e1e9a6772a3f7cbdeb8b49b3a208
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-16 16:38:55 +0000

    Replace goenv-go 1.4 with 1.4.1

--- modules/ci_environment/manifests/jenkins_job_support.pp
+++ modules/ci_environment/manifests/jenkins_job_support.pp
@@ -46,9 +46,14 @@ class ci_environment::jenkins_job_support {
   class { 'goenv':
     global_version => '1.2.2',
   }
-  goenv::version { ['1.2.2', '1.3.1', '1.3.3', '1.4']: }
+  goenv::version { ['1.2.2', '1.3.1', '1.3.3', '1.4.1']: }
   package { ['golang-gom', 'godep']:
     ensure => latest,
+  }
+
+  # FIXME: remove once run everywhere.
+  package { 'goenv-go-1.4':
+    ensure => purged,
   }
 
   # Needed to notify github of build statuses
