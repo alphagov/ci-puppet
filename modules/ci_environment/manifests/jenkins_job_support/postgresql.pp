9c82d2411050fb7f7eeea4e03147e45748c798bb
Murray Steele <muz@h-lame.com>
2015-11-06 11:33:30 +0000

    Move mapit postgres role password to hierdata

--- modules/ci_environment/manifests/jenkins_job_support/postgresql.pp
+++ modules/ci_environment/manifests/jenkins_job_support/postgresql.pp
@@ -1,6 +1,8 @@
 # == Class: ci_environment::jenkins_job_support::postgresql
 # Installs postgresql on the server
-class ci_environment::jenkins_job_support::postgresql {
+class ci_environment::jenkins_job_support::postgresql (
+  $mapit_role_password,
+) {
   include postgresql::server
   include postgresql::server::contrib
   include postgresql::lib::devel
@@ -19,7 +21,7 @@ class ci_environment::jenkins_job_support::postgresql {
   postgresql::server::role {
     'mapit':
       superuser     => true,
-      password_hash => postgresql_password('mapit', 'mapit');
+      password_hash => postgresql_password('mapit', $mapit_role_password);
   }
 
 }
