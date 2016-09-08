d510ab65821fc2e9edf4b58c6f23480f3a27e35f
Tim Blair <tim@bla.ir>
2016-09-08 07:22:40 +0100

    Add Go 1.7.1 and set as default
    Also remove old, unsupported versions of Go.

--- modules/ci_environment/manifests/jenkins_job_support.pp
+++ modules/ci_environment/manifests/jenkins_job_support.pp
@@ -49,9 +49,13 @@ class ci_environment::jenkins_job_support {
   }
 
   class { 'goenv':
-    global_version => '1.7',
+    global_version => '1.7.1',
   }
-  goenv::version { ['1.3.3', '1.4.2', '1.4.3', '1.5.1', '1.5.3', '1.6.2', '1.6.3', '1.7']: }
+  goenv::version { ['1.6.3', '1.7', '1.7.1']: }
+
+  goenv::version { ['1.3.3', '1.4.2', '1.4.3', '1.5.1', '1.5.3', '1.6.2']:
+    ensure => absent,
+  }
 
   package { ['golang-gom', 'godep']:
     ensure => latest,
